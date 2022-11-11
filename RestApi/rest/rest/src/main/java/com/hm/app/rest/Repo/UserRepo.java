4be0bbe7ed54d27d3b01d5ad70fef2c2182aaa77
Link1917 <91824319+Link1917@users.noreply.github.com>
2022-11-11 17:23:29 +0100

    Add files via upload

---
+++ RestApi/rest/rest/src/main/java/com/hm/app/rest/Repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.hm.app.rest.Repo;
+
+import com.hm.app.rest.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository <User, Long> {
+}
